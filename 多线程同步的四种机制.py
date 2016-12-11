67eba41e29ab238be1958cd44833b5242bcf84f6
Du,Shibing <shibingd123@163.com>
2016-12-11 21:50:48 +0800

    multhreading

---
+++ 多线程同步的四种机制.py
@@ -0,0 +1,62 @@
+# _*_coding:utf-8_*_
+__author__ = 'dushibing'
+
+# import threading
+# import time
+#
+# class Num:
+#     def __init__(self):
+#         self.num = 0
+#         self.lock = threading.Lock()
+#     def add(self):
+#         self.lock.acquire()#加锁，锁住相应的资源
+#         self.num += 1
+#         num = self.num
+#         self.lock.release()#解锁，离开该资源
+#         return num
+#
+# n = Num()
+# class jdThread(threading.Thread):
+#     def __init__(self,item):
+#         threading.Thread.__init__(self)
+#         self.item = item
+#     def run(self):
+#         time.sleep(2)
+#         value = n.add()#将num加1，并输出原来的数据和+1之后的数据
+#         print(self.item,value)
+#
+# for item in range(10):
+#     t = jdThread(item)
+#     t.start()
+#     t.join()#使线程一个一个执行
+
+
+import threading
+import time
+class Num:
+    def __init__(self):
+        self.num = 0
+        self.sem = threading.Semaphore(value = 3)
+        #允许最多三个线程同时访问资源
+
+    def add(self):
+        self.sem.acquire()#内部计数器减1
+        self.num += 1
+        num = self.num
+        self.sem.release()#内部计数器加1
+        return num
+
+n = Num()
+class jdThread(threading.Thread):
+    def __init__(self, item):
+        threading.Thread.__init__(self)
+        self.item = item
+    def run(self):
+        time.sleep(2)
+        value = n.add()
+        print(self.item,value)
+
+for item in range(10):
+    t = jdThread(item)
+    t.start()
+    t.join()

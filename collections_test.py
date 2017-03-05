023032cc966d573acd94b6b0240c7b573fb02b47
Scott Du <shibingd123@163.com>
2017-03-05 21:48:57 +0800

    OrderedDict

--- collections_test.py
+++ collections_test.py
@@ -9,3 +9,25 @@ print("fifo.pop() {0}".format(fifo.pop()))
 print("fifo {0}".format(fifo))
 print("fifo.popleft() {0}".format(fifo.popleft()))
 print("fifo {0}".format(fifo))
+a = collections.OrderedDict
+
+
+'''
+In [4]: a = collections.OrderedDict()
+
+In [5]: a['foo'] = 1
+
+In [6]: a['bar'] = 2
+
+In [7]: b = collections.OrderedDict()
+
+In [8]: b['foo'] = 'red'
+
+In [9]: b['bar'] = 'blue'
+
+or values1, values2 in zip(a.values(), b.values()):
+    ...:     print (values1, values2)
+    ...:     
+(1, 'red')
+(2, 'blue')
+'''
